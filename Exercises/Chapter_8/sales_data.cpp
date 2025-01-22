7763c6afbe6dd9aada0670f94592899f383f7d7f
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-22 12:39:55 +0100

    Add sales_data class

---
+++ Exercises/Chapter_8/sales_data.cpp
@@ -0,0 +1,38 @@
+#include "sales_data.h"
+#include <stdexcept>
+#include <iostream>
+#include <string>
+
+using std::invalid_argument;
+using std::runtime_error;
+using std::cin;
+using std::cout;
+using std::string;
+
+sales_data::sales_data(std::istream &is)
+{
+    std::string book_no;
+    int units_sold = 0;
+    double revenue = 0.0;
+
+    is >> book_no >> units_sold >> revenue;
+    if(!is) throw std::runtime_error("Error: Could not parse input to Sales_data.");
+
+    this->book_no = book_no;
+    this->units_sold = units_sold;
+    this->revenue = revenue;
+}
+
+void sales_data::add(const sales_data &data)
+{
+    if(book_no != data.book_no)
+        throw invalid_argument("Cannot combine book data with different ISBN.");
+
+    units_sold += data.units_sold;
+    revenue += data.revenue;
+}
+
+std::string sales_data::to_string() const
+{
+    return "[" + book_no + " " + std::to_string(units_sold) + " " + std::to_string(revenue) + "]";
+}
---
+++ Exercises/Chapter_8/sales_data.h
@@ -0,0 +1,27 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <iostream>
+#include <string>
+
+class sales_data
+{
+    public:
+        sales_data() = default;
+        explicit sales_data(const std::string &book_no) : book_no(book_no) {}
+        sales_data(const std::string &book_no, int units_sold, double revenue) : book_no(book_no), units_sold(units_sold), revenue(revenue) {}
+        explicit sales_data(std::istream &is);
+
+        void add(const sales_data &data);
+        const std::string &get_book_no() const { return book_no; }
+        std::string to_string() const;
+
+    private:
+        std::string book_no;
+        int units_sold = 0;
+        double revenue = 0.0;
+
+        double get_average_price() const { return revenue / units_sold; }
+};
+
+#endif //SALES_DATA_H
