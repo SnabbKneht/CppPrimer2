21a7942c3cf4e7fbeed5bc5da24ccce96e138474
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-27 15:23:52 +0100

    Add ex 9.4

--- Exercises/Chapter_9/Ex9_2.cpp
+++ Exercises/Chapter_9/Ex9_2.cpp
@@ -1,10 +1,32 @@
+#include <iostream>
+#include <vector>
 #include <list>
 #include <deque>
 
+using std::cout;
+using std::vector;
 using std::list;
 using std::deque;
 
+bool find(vector<int>::iterator beg, vector<int>::iterator end, int value)
+{
+    while(beg != end)
+    {
+        if(*beg == value) return true;
+        ++beg;
+    }
+    return false;
+}
+
 int main()
 {
-    list<deque<int>> x;
+    // Exercise 9.2
+
+    list<deque<int>> example;
+
+    // Exercise 9.4
+
+    vector vec {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << (find(vec.begin(), vec.end(), 3) ? "Found" : "Not found") << '\n';
+    cout << (find(vec.begin(), vec.end(), 13) ? "Found" : "Not found") << '\n';
 }
