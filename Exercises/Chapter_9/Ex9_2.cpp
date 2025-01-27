f7e786c04cf7657b10a6e88ce77bd136192e8b83
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-27 16:27:58 +0100

    Add ex 9.10

--- Exercises/Chapter_9/Ex9_2.cpp
+++ Exercises/Chapter_9/Ex9_2.cpp
@@ -45,4 +45,13 @@ int main()
     cout << '\n';
     cout << (ifind(vec.begin(), vec.end(), 3) != vec.end() ? "Found" : "Not found") << '\n';
     cout << (ifind(vec.begin(), vec.end(), 13) != vec.end() ? "Found" : "Not found") << '\n';
+
+    // Exercise 9.10
+
+    vector<int> v1;
+    const vector<int> v2;
+    auto it1 = v1.begin();
+    auto it2 = v2.begin();
+    auto it3 = v1.cbegin();
+    auto it4 = v2.cbegin();
 }
