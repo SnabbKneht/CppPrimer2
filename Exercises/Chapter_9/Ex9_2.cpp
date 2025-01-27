ec8c834cad796aa87134ccf2d4b7b36887a76597
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-27 15:46:12 +0100

    Add ex 9.5

--- Exercises/Chapter_9/Ex9_2.cpp
+++ Exercises/Chapter_9/Ex9_2.cpp
@@ -18,6 +18,16 @@ bool find(vector<int>::iterator beg, vector<int>::iterator end, int value)
     return false;
 }
 
+vector<int>::iterator ifind(vector<int>::iterator beg, vector<int>::iterator end, int value)
+{
+    while(beg != end)
+    {
+        if(*beg == value) return beg;
+        ++beg;
+    }
+    return end;
+}
+
 int main()
 {
     // Exercise 9.2
@@ -29,4 +39,10 @@ int main()
     vector vec {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << (find(vec.begin(), vec.end(), 3) ? "Found" : "Not found") << '\n';
     cout << (find(vec.begin(), vec.end(), 13) ? "Found" : "Not found") << '\n';
+
+    // Exercise 9.5
+
+    cout << '\n';
+    cout << (ifind(vec.begin(), vec.end(), 3) != vec.end() ? "Found" : "Not found") << '\n';
+    cout << (ifind(vec.begin(), vec.end(), 13) != vec.end() ? "Found" : "Not found") << '\n';
 }
