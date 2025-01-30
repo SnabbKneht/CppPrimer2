73a988a92f19953b3a6baf5f64fd7ec6e4608ae2
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-30 17:39:40 +0100

    Add ex 9.21

--- Exercises/Chapter_9/Ex9_20.cpp
+++ Exercises/Chapter_9/Ex9_20.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <list>
 #include <deque>
+#include <string>
+#include <vector>
 
+using std::cin;
 using std::cout;
 using std::list;
 using std::deque;
+using std::string;
+using std::vector;
 
 int main()
 {
+    // Exercise 9.20
+    cout << "\nExercise 9.20\n\n";
+
     list numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     deque<int> odd;
     deque<int> even;
@@ -28,6 +36,23 @@ int main()
     for(auto num : even)
     {
         cout << num << ' ';
+    }
+    cout << '\n';
+
+    // Exercise 9.21
+    cout << "\nExercise 9.21\n\n";
+
+    vector<string> lst;
+    string word;
+    auto iter = lst.begin();
+    while(cin >> word)
+    {
+        iter = lst.insert(iter, word);
+    }
+
+    for(auto &s : lst)
+    {
+        cout << s << ' ';
     }
     cout << '\n';
 }
