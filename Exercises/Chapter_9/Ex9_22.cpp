192ef7adab43adffc9963a58339029a3ba3f1d04
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-31 10:37:27 +0100

    Add ex 9.24

--- Exercises/Chapter_9/Ex9_22.cpp
+++ Exercises/Chapter_9/Ex9_22.cpp
@@ -6,6 +6,9 @@ using std::vector;
 
 int main()
 {
+    // Exercise 9.22
+    cout << "\nExercise 9.22\n\n";
+
     vector v = {1, 1, 1, 1, 1, 5, 5, 5, 5, 5};
     int some_val = 1;
 
@@ -23,4 +26,13 @@ int main()
         cout << i << ' ';
     }
     cout << '\n';
+
+    // Exercise 9.24
+    cout << "\nExercise 9.24\n\n";
+
+    v = {1, 2, 3, 4, 5};
+    cout << v.front() << '\n';  // undefined if empty
+    cout << *v.begin() << '\n'; // undefined if empty
+    cout << v[0] << '\n';       // undefined if empty
+    cout << v.at(0) << '\n'; // error if empty
 }
