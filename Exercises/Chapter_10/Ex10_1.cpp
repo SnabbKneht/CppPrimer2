9b64c9f5df2f68ff0c5de353afebd760f93632a6
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-02-02 14:09:03 +0100

    Add C++20 approach for ex 10.1

--- Exercises/Chapter_10/Ex10_1.cpp
+++ Exercises/Chapter_10/Ex10_1.cpp
@@ -10,4 +10,5 @@ int main()
 {
     vector numbers = {1, 2, 3, 4, 4, 5, 6, 7, 3, 3, 3, 8, 9, 10};
     cout << count(numbers.begin(), numbers.end(), 4) << '\n';
+    cout << std::ranges::count(numbers, 4) << '\n'; // new in C++20
 }
