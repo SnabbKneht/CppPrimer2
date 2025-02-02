859505809c1fe58675e04544d0fca76118d8843c
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-02-02 13:59:46 +0100

    Add ex 10.1

---
+++ Exercises/Chapter_10/Ex10_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::vector;
+using std::count;
+
+int main()
+{
+    vector numbers = {1, 2, 3, 4, 4, 5, 6, 7, 3, 3, 3, 8, 9, 10};
+    cout << count(numbers.begin(), numbers.end(), 4) << '\n';
+}
