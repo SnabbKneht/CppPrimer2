f794ced4a3fae50849dfc65f2b90e6bb8d0e6bee
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2025-02-03 15:30:17 +0100

    Add ex 10.4

--- Exercises/Chapter_10/Ex10_3.cpp
+++ Exercises/Chapter_10/Ex10_3.cpp
@@ -12,4 +12,10 @@ int main()
 
     vector numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << accumulate(numbers.cbegin(), numbers.cend(), 0) << '\n';
+
+    // Exercise 10.4
+
+    vector numbers2 = {1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1, 9.1, 10.1};
+    cout << accumulate(numbers2.cbegin(), numbers2.cend(), 0) << '\n'; // Ok, but values truncated to int
+    cout << accumulate(numbers2.cbegin(), numbers2.cend(), 0.0) << '\n'; // Better
 }
