672d19e352a12a62b26a1fb7b95fc0ac9020871c
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2025-02-15 12:45:54 +0100

    Add ex 10.15

--- Exercises/Chapter_10/Ex10_14.cpp
+++ Exercises/Chapter_10/Ex10_14.cpp
@@ -4,6 +4,15 @@ using std::cout;
 
 int main()
 {
+    // Exercise 10.14
+    cout << "\nExercise 10.14\n\n";
+
     auto sum = [](int a, int b) { return a + b; };
     cout << sum(5, 3) << '\n';
+
+    // Exercise 10.15
+    cout << "\nExercise 10.15\n\n";
+
+    int x = 4;
+    cout << [x](int i) { return x + i; }(1) << '\n'; // Inline declaration and call
 }
