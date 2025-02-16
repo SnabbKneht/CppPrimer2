dbb10efeb4cca001488c1a23c31e84d8f2153a2f
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2025-02-16 13:51:57 +0100

    Add ex 10.25

--- Exercises/Chapter_10/Ex10_16.cpp
+++ Exercises/Chapter_10/Ex10_16.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <functional>
 
 using std::cout;
 using std::ifstream;
@@ -13,10 +14,17 @@ using std::ranges::stable_sort;
 using std::unique;
 using std::ranges::stable_partition;
 using std::ranges::for_each;
+using std::bind;
+using std::placeholders::_1;
 
 bool is_shorter(const string &a, const string &b)
 {
     return a.size() < b.size();
+}
+
+bool is_longer_or_equal(const string &s, int i)
+{
+    return s.size() >= i;
 }
 
 void sort_and_elim_dups(vector<string> &words)
@@ -38,9 +46,16 @@ void biggies(vector<string> &words, vector<string>::size_type min_size)
     sort_and_elim_dups(words);
     sort_by_size(words);
 
+    // Lambda approach (preferred)
     auto iter = stable_partition(words, [min_size](const string &s)
         { return s.size() >= min_size; }).begin();
     words.erase(iter, words.end());
+
+    // The same thing using bind, instead of lambda:
+
+    // auto iter = stable_partition(words,
+    //     bind(is_longer_or_equal, _1, min_size)).begin();
+    // words.erase(iter, words.end());
     
     cout << "There are " << words.size() << " elements with size >= " << min_size << ":\n";
     for_each(words, [](const string &s) { cout << s << ' '; });
