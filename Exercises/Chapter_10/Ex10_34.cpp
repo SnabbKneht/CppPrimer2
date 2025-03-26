d38d901d27906c7b151816a755d45cac0290a740
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2025-03-26 19:01:44 +0100

    Add ex 10.35

--- Exercises/Chapter_10/Ex10_34.cpp
+++ Exercises/Chapter_10/Ex10_34.cpp
@@ -6,8 +6,16 @@ using std::vector;
 
 int main()
 {
+    // Exercise 10.34
+
     const vector numbers = {1, 2, 3, 4, 5};
     for(auto iter = numbers.crbegin(); iter != numbers.crend(); ++iter)
         cout << *iter << ' ';
     cout << '\n';
+
+    // Exercise 10.35
+
+    for(auto iter2 = numbers.cend() - 1; iter2 != numbers.cbegin(); --iter2)
+        cout << *iter2 << ' ';
+    cout << *numbers.cbegin() << '\n';
 }
