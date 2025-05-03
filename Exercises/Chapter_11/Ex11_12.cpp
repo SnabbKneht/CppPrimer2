1b4fcd4257b51516b30295ade099a7bb6f16a2b0
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2025-05-03 19:24:26 +0200

    Add ex 11.12

---
+++ Exercises/Chapter_11/Ex11_12.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <utility>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::pair;
+using std::string;
+using std::vector;
+using std::ranges::for_each;
+
+int main()
+{
+    vector<pair<string, int>> pairs;
+
+    string s;
+    int i;
+    while(cin >> s >> i)
+    {
+        pairs.emplace_back(s, i);
+    }
+
+    // Using lambda
+    for_each(pairs, [](const auto &pair)
+    {
+        cout << pair.first << ' ' << pair.second << '\n';
+    });
+
+    cout << '\n';
+
+    // The same thing using structured bindings and normal range-based for loop
+    for(const auto &[first, second] : pairs)
+    {
+        cout << first << ' ' << second << '\n';
+    }
+}
