f7abe3535e8b26636aef3a58903182262307b011
Jakub Kopczewski <kuba.kopczewsky@gmail.com>
2025-06-08 11:40:30 +0200

    Add ex 11.29

--- Exercises/Chapter_11/Ex11_28.cpp
+++ Exercises/Chapter_11/Ex11_28.cpp
@@ -22,4 +22,17 @@ int main()
     {
         cout << i << '\n';
     });
+
+    auto iter_1 = m.lower_bound("z");
+    auto iter_2 = m.upper_bound("z");
+    auto iter_range = m.equal_range("a");
+    // Returns iterators to "abc" because
+    // "abc" is the first key not less and greater that "a"
+    auto iter_3 = iter_range.first;
+    auto iter_4 = iter_range.second;
+
+    cout << (iter_1 == m.end() ? "Off the end" : "Other") << '\n';
+    cout << (iter_2 == m.end() ? "Off the end" : "Other") << '\n';
+    cout << (iter_3 == m.end() ? "Off the end" : "Other") << '\n';
+    cout << (iter_4 == m.end() ? "Off the end" : "Other") << '\n';
 }
